f04ea9a2e0f54c67a57044dbc564c13344c126f4
nhattannn222 <nhnt205@gmail.com>
2024-09-21 20:29:47 +0700

    Fix API + Changed to .env

--- index.js
+++ index.js
@@ -1,48 +1,62 @@
 const express = require('express');
 const { Pool } = require('pg');
-const cors = require('cors');
+const dotenv = require('dotenv');
+
+// Load environment variables from .env file
+dotenv.config();
 
 const app = express();
 app.use(express.json());
-const port = 4000;
+const port = process.env.PORT;
 
-// Cấu hình kết nối PostgreSQL
+// Cấu hình kết nối PostgreSQL bằng biến môi trường từ .env
 const pool = new Pool({
-    user: 'b1d72ac3-a069-4182-825b-809fe72df8c9',         // Thay bằng tên người dùng của bạn
-    host: '52.163.226.41',              // Địa chỉ máy chủ PostgreSQL
-    database: 'e4f1625f-498b-457f-8718-8d8f03cc9ce0',      // Thay bằng tên cơ sở dữ liệu của bạn
-    password: 'JSi3MYRL2gkAt8yYuoUoh9Ssu',      // Thay bằng mật khẩu của bạn
-    port: 5432,                     // Cổng PostgreSQL
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
 });
 
-
-
-// API để lấy dữ liệu từ bảng sensor_Data
-app.get('/api/sensors/latest', async (req, res) => {
+// API để lấy dữ liệu từ bảng sensor_data
+app.get('/api/sensors', async (req, res) => {
     try {
-        const result = await pool.query('SELECT * FROM my_schema.sensor_data ORDER BY timestamp DESC LIMIT 1');
-        res.json(result.rows);
+        const result = await pool.query('SELECT * FROM my_schema.sensor_data ');
+        res.status(200).json(result.rows);
     } catch (err) {
         console.error(err);
-        res.status(500).send('Error retrieving data');
+        res.status(500).json('Error retrieving data');
     }
 });
 
 app.get('/api/sensors/latest/:deviceId', async (req, res) => {
-    const { deviceId } = req.params; // Lấy device_id từ URL
+    const { deviceId } = req.params;
 
     try {
         const result = await pool.query(
             'SELECT * FROM my_schema.sensor_data WHERE device_id = $1 ORDER BY timestamp DESC LIMIT 1',
             [deviceId]
         );
-        res.json(result.rows);
+
+        if (result.rows.length === 0) {
+            return res.status(404).json('Sensor data not found');
+        }
+        res.status(200).json(result.rows);
     } catch (err) {
         console.error(err);
-        res.status(500).send('Error retrieving data');
+        res.status(500).json('Error retrieving data');
     }
 });
 
+
+app.get('/api/health', async (req, res) => {
+    try {
+        res.status(200).json();
+    } catch (err) {
+        console.error(err);
+        res.status(500).json('Error retrieving data');
+    }
+});
 
 // Khởi động server
 app.listen(port, () => {
